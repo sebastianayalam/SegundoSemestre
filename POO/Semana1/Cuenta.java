a4818b3693f62b4f19e502a037f9277ccb5d790a
sebastianayalam <sebastian.ayalam@campusucc.edu.co>
2026-09-16 21:45:20 +0000

    actividd mejorda

--- POO/Semana1/Cuenta.java
+++ POO/Semana1/Cuenta.java
@@ -1,26 +1,37 @@
  public class Cuenta {
-  
-  //Atributos
+
+  // Atributos
   private int cedula;
   private String nombre;
   private String numeroCuenta;
   private String tipoCuenta;
   private double saldo;
-  
-  //El constructor de la clase permite inicializar la clase
-  //El constructor de la clase se reconoce porque tiene el mismo nombre de la clase
-  public Cuenta(int cedula, String nombre, String numeroCuenta, String tipoCuenta, double saldo){
+
+  // El constructor de la clase permite inicializar la clase
+  // El constructor de la clase se reconoce porque tiene el mismo nombre de la
+  // clase
+  public Cuenta(int cedula, String nombre, String numeroCuenta, String tipoCuenta, double saldo) {
     this.cedula = cedula;
-    this.nombre= nombre;
+    this.nombre = nombre;
     this.numeroCuenta = numeroCuenta;
     this.tipoCuenta = tipoCuenta;
     this.saldo = saldo;
   }
-  
-  public String toString(){
-    return "Cuenta [ cedula:" + cedula + " nombre: " + nombre + " numerocuenta: " + numeroCuenta + 
-                     " tipoCuenta: " + tipoCuenta + " saldo: " + saldo + "]";
-  }
-}  
 
-//*********************************************************
+  // Creación del método recargar
+  public void recargarCuenta(double cantidad) {
+    saldo += cantidad;
+  }
+
+  /// retirar saldo de cuenta
+  public void retirarCuenta(double cantidad) {
+  saldo -= cantidad;
+   }
+  
+  public String toString() {
+    return "Cuenta [ cedula:" + cedula + " nombre: " + nombre + " numerocuenta: " + numeroCuenta +
+        " tipoCuenta: " + tipoCuenta + " saldo: " + saldo + "]";
+  }
+}
+
+// *******************
--- POO/Semana1/EjecutarBanco.java
+++ POO/Semana1/EjecutarBanco.java
@@ -1,11 +1,20 @@
 public class EjecutarBanco {
-  public static void main(String[] args){
-    
-    //Creación del objeto
+  public static void main(String[] args) {
+
+    // Creación del objeto
     Cuenta objCuenta1 = new Cuenta(1010, "Juan", "587-3", "Ahorros", 0.0);
-    
+
     System.out.println(objCuenta1.toString());
+
+    objCuenta1.recargarCuenta(10000.0);
+
+    System.out.println(objCuenta1.toString());
+
+    System.out.println(objCuenta1.toString());
+
+    objCuenta1.retirarCuenta(10000.0);
+
+    System.out.println(objCuenta1.toString());
+
   }
-} 
-
-
+  }
